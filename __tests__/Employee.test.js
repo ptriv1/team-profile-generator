ebb3ba6ce35bf4ec0525c5df3a367ae4aa8784a3
Galatia <ptrivedius@aol.com>
2021-08-27 21:39:41 -0400

    tests in Employee.test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,31 @@
-const employee = require('../lib/employee');
+const Employee = require('../lib/employee');
+
+describe("Employee", () => {
+    test("Can instantiate Employee instance", () => {
+        const e = new Employee();
+        expect(typeof e).toBe("object");
+    });
+
+    test("Can set name via constructor arguments", () => {
+        const name = "Alice";
+        const e = new Employee(name);
+        expect(e.name).toBe(name);
+    });
+
+    test("Can set id using the constructor", () => {
+        const value = 100;
+        const e = new Employee("value", value);
+        expect(e.id).toBe(value);
+    });
+
+    test("Can set email using the constructor", () => {
+        const value = "test@test.com";
+        const e = new Employee("email", 1, value);
+    });
+
+    test("Can get name via getName()", () => {
+        const value = "Alice";
+        const e = new Employee(value);
+        expect(e.getName()).toBe(value);
+    });
+});
