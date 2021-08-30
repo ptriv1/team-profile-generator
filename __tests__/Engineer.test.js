00110e3603aae7b6f222489018668b981ee483cb
Galatia <ptrivedius@aol.com>
2021-08-30 13:36:14 -0400

    pushing current code

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,15 @@
+const Engineer = require('../lib/engineer');
 const engineer = require('../lib/engineer');
+
+describe("Engineer", () => {
+    test("Can instantiate Engineer instance", () => {
+        const e = new Engineer();
+        expect(typeof e).toBe("object");
+    });
+
+    test("Can get role via getRole()", () => {
+        const value = "Engineer";
+        const e = new Engineer(value);
+        expect(e.getRole()).toBe(value);
+    });
+});
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,15 @@
+const Intern = require('../lib/intern');
 const intern = require('../lib/intern');
+
+describe("Intern", () => {
+    test("Can instantiate Intern object", () => {
+        const e = new Intern();
+        expect(typeof e).toBe("object");
+    });
+
+    test("Can get role via getRole()", () => {
+        const value = "Intern";
+        const e = new Intern(value);
+        expect(e.getRole()).toBe(value);
+    });
+});
