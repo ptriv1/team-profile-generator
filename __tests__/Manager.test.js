db484ed5b790a51121a3ef69071c6228a0185e37
Galatia <ptrivedius@aol.com>
2021-08-30 11:20:05 -0400

    tests in test.js file

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,14 @@
-const manager = require('../lib/manager');
+const Manager = require('../lib/manager');
+
+describe("Manager", () => {
+    test("Can instantiate Manager instance", () => {
+        const e = new Manager();
+        expect(typeof e).toBe("object");
+    });
+
+    test("Can get role via getRole()", () => {
+        const value = "Manager";
+        const e = new Manager(value);
+        expect(e.getRole()).toBe(value);
+    });
+});
