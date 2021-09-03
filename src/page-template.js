78f41bc4ab044352bb7475c50bda43de022dd8e4
Galatia <ptrivedius@aol.com>
2021-09-03 13:46:18 -0400

    reverting to earlier HTML formatting

--- src/page-template.js
+++ src/page-template.js
@@ -83,6 +83,13 @@ module.exports = team => {
 </head>
 
 <body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 jumbotron mb-3 team-heading">
+                <h1 class="text-center">Team Profiles</h1>
+            </div>
+        </div>
+    </div>
     <div class="container">
         <div class="row">
             <div class="team-area col-12 d-flex justify-content-center">
