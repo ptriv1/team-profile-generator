a2c3b5b711d581413e8a8c21e77d8eb6e7d36bf7
Galatia <ptrivedius@aol.com>
2021-09-04 14:49:02 -0400

    pushing current code

--- src/page-template.js
+++ src/page-template.js
@@ -4,7 +4,7 @@ const createManager = manager => {
     return `
         <div class="manager-section">
         <h2 class="card-title">${manager.getName()}</h2>
-            <ul class="employee-list">
+            <ul class="col-1-3 employee-list">
                 <li class="list-item">ID: ${manager.getId()}</li>
                 <li class="list-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
                 <li class="list-item">Office number: ${manager.getOfficeNumber()}</li>
@@ -17,7 +17,7 @@ const createEngineer = engineer => {
     return `
         <div class="engineer-section">
         <h2 class="card-title">${engineer.getName()}</h2>
-            <ul class="employee-list">
+            <ul class="col-1-3 employee-list">
                 <li class="list-item">ID: ${engineer.getId()}</li>
                 <li class="list-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
                 <li class="list-item">GitHub: <a href="http://github.com/${engineer.getGithub()}" target="_blank">${engineer.getName()}</a></li>
@@ -30,7 +30,7 @@ const createIntern = intern => {
     return `
         <div class="intern-section">
         <h2 class="card-title">${intern.getName()}</h2>
-            <ul class="employee-list">
+            <ul class="col-1-3 employee-list">
                 <li class="list-item">ID: ${intern.getId()}</li>
                 <li class="list-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
                 <li class="list-item">School: ${intern.getSchool()}</li>
@@ -75,20 +75,22 @@ module.exports = team => {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Team Profiles</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <div class="page-heading">
         <div class="row">
-            <div class="team-heading">
+            <div class="col-12 mb-3 team-heading">
                 <h1 class="text-center">Team Profiles</h1>
             </div>
         </div>
     </div>
     <div class="container">
         <div class="row">
-            <div class="justify-content">
+            <div class"team-area justify-content-center"=>
                 ${generateTeam(team)}
             </div>
         </div>
